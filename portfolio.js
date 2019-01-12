08c16fafe28c5e26ed75973fb8862df4aa1e95f8
Aaron Shivers <aaron@moonmail.org>
2019-01-12 07:11:55 -0600

    Added css file, and some placeholder images

--- portfolio.js
+++ portfolio.js
@@ -3,6 +3,10 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000
 
-app.get('/', (req, res) => res.send('Hello Everybody!'))
+app.use(express.static('public'))
+
+app.get('/', (req, res) => {
+  res.render('index')
+})
 
 app.listen(port)
