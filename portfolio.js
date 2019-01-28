5d611648074bbe316637c832a130253846a49e01
Aaron Shivers <aaron@moonmail.org>
2019-01-28 08:23:56 -0600

    Added error routes

--- portfolio.js
+++ portfolio.js
@@ -9,4 +9,12 @@ app.get('/', (req, res) => {
   res.render('index')
 })
 
+app.use((req, res, next) => {
+  res.status(404).send('Sorry, we cannot find that!')
+})
+
+app.use((err, req, res, next) => {
+  res.status(500).send('Something broke!')
+})
+
 app.listen(port)
