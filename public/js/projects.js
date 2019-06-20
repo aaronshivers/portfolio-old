593d58f7be36082bc8b7a66ce7a01d21be885087
aaronshivers <aaron@moonmail.org>
2019-06-20 08:09:00 -0500

    added projects.js file

---
+++ public/js/projects.js
@@ -0,0 +1,21 @@
+const projects = [{
+  name: 'chat app',
+  link: 'http://chat.aaronshivers.com/',
+  github: 'https://github.com/aaronshivers/node-chat-app',
+  description: 'This is a group chat application. It uses socket.io to keep the connection to the server open for updates.'
+}, {
+  name: 'weather app',
+  link: 'http://weather.aaronshivers.com',
+  github: 'https://github.com/aaronshivers/weather',
+  description: 'This is an application for checking the weather. It uses the Google Geocoding API for the location search, then passes that to the DarkSky API for the weather.'
+}, {
+  name: 'timesheet',
+  link: 'http://timesheet.aaronshivers.com/',
+  github: 'https://github.com/aaronshivers/timesheet',
+  description: 'A single page application for tracking time spent on jobs. This uses JavaScript without any front-end frameworks.'
+}, {
+  name: 'job Estimator',
+  link: 'http://estimator.aaronshivers.com/',
+  github: 'https://github.com/aaronshivers/estimator',
+  description: 'This is a simple application for estimating or pricing jobs based on the dual overhead method. The user can sign up and save their settings for future use.'
+}]
